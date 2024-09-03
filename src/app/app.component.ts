15898a10652bd53ca0ad3aed73722513089dfb97
arthur <arthur.almeida51@etec.sp.gov.br>
2024-09-02 21:27:12 -0300

    versão 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PrimeiroComponenteComponent } from "./primeiro-componente/primeiro-componente.component";
+import { SegundoComponenteComponent } from "./segundo-componente/segundo-componente.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, PrimeiroComponenteComponent, SegundoComponenteComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'terceiro-projeto';
+}
---
+++ src/app/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-segundo-componente',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './segundo-componente.component.html',
+  styleUrl: './segundo-componente.component.css'
+})
+export class SegundoComponenteComponent {
+
+  nomes:string='';
+  cidade:string="";
+
+}
